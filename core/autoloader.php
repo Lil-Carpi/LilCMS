4af30f7c75f57f10b284cc31ee48a9e07787c665
Lil-Carpi <duartesebas007@gmail.com>
2025-11-03 23:41:49 +0100

    Created General autoloader

--- core/autoloader.php
+++ core/autoloader.php
@@ -10,7 +10,7 @@ spl_autoload_register(function($class) { // definir autoloader
     if (strncmp($prefix, $class, $len) === 0) { // verifica si la clase pertenece al namespace comparando los caracteres de $class con $prefix
                                                 // ejemplo: $class = 'App\Controllers\HomeController' y $prefix = 'App\\': la comparacion devuelve o deberia devolver 0 => coinciden.
       $relative_class = substr($class, $len);   // extrae la parte relativa de la clase, sin el namespace
-      $file = $base_dir . str_replace('\\', '/' $relative_class) . '.php'; // hace la conversion de los namespace en rutas usando "/" y concatena con base_dir y ".php"
+      $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php'; // hace la conversion de los namespace en rutas usando "/" y concatena con base_dir y ".php"
       if (file_exists($file)) require_once $file; // si el fichero existe, carga a memoria.
     }
   }
