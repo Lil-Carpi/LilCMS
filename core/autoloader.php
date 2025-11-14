a08dd91ec580bfa5f3d73456f74468d33fc98443
Lil_Carpi <carpi@mailcarpi.xyz>
2025-11-14 15:51:31 +0100

    Testing VSCode + GitHub.

--- core/autoloader.php
+++ core/autoloader.php
@@ -8,10 +8,12 @@ spl_autoload_register(function($class) { // definir autoloader
   foreach ($map as $prefix => $base_dir) {  // se recorre el mapa de namespaces
     $len = strlen($prefix);
     if (strncmp($prefix, $class, $len) === 0) { // verifica si la clase pertenece al namespace comparando los caracteres de $class con $prefix
-                                                // ejemplo: $class = 'App\Controllers\HomeController' y $prefix = 'App\\': la comparacion devuelve o deberia devolver 0 => coinciden.
+ // ejemplo: $class = 'App\Controllers\HomeController' y $prefix = 'App\\': la comparacion devuelve o deberia devolver 0 => coinciden.
       $relative_class = substr($class, $len);   // extrae la parte relativa de la clase, sin el namespace
       $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php'; // hace la conversion de los namespace en rutas usando "/" y concatena con base_dir y ".php"
       if (file_exists($file)) require_once $file; // si el fichero existe, carga a memoria.
     }
   }
 });
+
+?>
