d6d0d4ac0b3b0486ba12813fb2dae04484bc99fa
Lil Carpi <duartesebas007@gmail.com>
2026-04-15 01:16:23 +0200

    chore: restructure folders and setup Django templates in settings.py

--- core/settings.py
+++ core/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS: [os.path.join(BASE_DIR, 'static')]
 
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
